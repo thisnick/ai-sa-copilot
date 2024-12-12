2150df07714345b57065b9bf29e4cc438ccf1726
Nico Albanese <nico.albanese@vercel.com>
2024-12-12 18:19:05 +0000

    update to version 4

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -1,7 +1,7 @@
 import { createResource } from "@/lib/actions/resources";
 import { findRelevantContent } from "@/lib/ai/embedding";
 import { openai } from "@ai-sdk/openai";
-import { convertToCoreMessages, generateObject, streamText, tool } from "ai";
+import { generateObject, streamText, tool } from "ai";
 import { z } from "zod";
 
 // Allow streaming responses up to 30 seconds
@@ -10,9 +10,9 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const result = await streamText({
+  const result = streamText({
     model: openai("gpt-4o"),
-    messages: convertToCoreMessages(messages),
+    messages,
     system: `You are a helpful assistant acting as the users' second brain.
     Use tools on every request.
     Be sure to getInformation from your knowledge base before answering any questions.
